79631a3e9445f5e9eae36caceaacab904c5ed603
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-26 23:29:49 +0500

    Add DTOs

---
+++ StaySphere.Api/StaySphere.Domain/DTOs/Room/RoomForUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaySphere.Domain.DTOs.Room
+{
+    public record RoomForUpdateDto(
+        int Id,
+        int CategoryId,
+        int Number,
+        int Floor);
+
+}
