e36bb4da6e0918afd212238b166da3bb6a997652
axamtovdiyorbek <128776299+diyorbekaxmatov@users.noreply.github.com>
2023-12-29 21:16:04 +0500

    Add-Pagination

---
+++ StaySphere.Api/StaySphere.Domain/Pagination/PaginatedList.cs
@@ -0,0 +1,23 @@
+﻿
+namespace StaySphere.Domain.Pagination
+{
+    public class PaginatedList<T> : List<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(items);
+        }
+    }
+}
---
+++ StaySphere.Api/StaySphere.Domain/Pagination/PaginationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using StaySphere.Domain.Common;
+
+namespace StaySphere.Domain.Pagination
+{
+    public static class PaginationExtensions
+    {
+        public static PaginatedList<T> ToPaginatedList<T>(
+            this IQueryable<T> source,
+            int pageSize,
+            int pageNumber) where T : EntityBase
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
