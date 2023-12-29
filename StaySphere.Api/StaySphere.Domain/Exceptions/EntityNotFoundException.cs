a935f7e12cca4049766608899eda9c9086b9d6f2
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-29 22:30:07 +0500

    Create Exeption

---
+++ StaySphere.Api/StaySphere.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿namespace StaySphere.Domain.Exeptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException()
+        {
+        }
+
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
