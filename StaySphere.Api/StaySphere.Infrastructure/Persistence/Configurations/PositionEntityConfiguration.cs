b08505324cbc74d863a91b5786aa5a1ff2b48ad2
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-31 20:41:00 +0500

    Add PositionEntityConfiguration

--- StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/PositionEntityConfiguration.cs
+++ StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/PositionEntityConfiguration.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StaySphere.Domain.Entities;
 
 namespace StaySphere.Infrastructure.Persistence.Configurations
 {
-    internal class PositionEntityConfiguration
+    public class PositionEntityConfiguration : IEntityTypeConfiguration<Position>
     {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.ToTable(nameof(Position));
+            builder.HasKey(p => p.Id);
+
+            builder.HasMany(p => p.Employees)
+                .WithOne(e => e.Position)
+                .HasForeignKey(e => e.PositionId);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(255);
+            builder.Property(p => p.Salary)
+                .HasColumnType("money");
+        }
     }
 }
