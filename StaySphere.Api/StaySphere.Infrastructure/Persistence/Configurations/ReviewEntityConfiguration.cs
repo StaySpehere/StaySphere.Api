f12535b19c63e7bcab41213dcdc6bc1e086d968b
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-31 20:46:49 +0500

    Add ReviewEntityConfiguration and change Review's grade type

--- StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/ReviewEntityConfiguration.cs
+++ StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/ReviewEntityConfiguration.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StaySphere.Domain.Entities;
 
 namespace StaySphere.Infrastructure.Persistence.Configurations
 {
-    internal class ReviewEntityConfiguration
+    public class ReviewEntityConfiguration : IEntityTypeConfiguration<Review>
     {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.ToTable(nameof(Review));
+            builder.HasKey(b => b.Id);
+
+            builder.HasOne(r => r.Booking)
+                 .WithMany(b => b.Reviews)
+                 .HasForeignKey(r => r.BookingId);
+
+            builder.Property(r => r.Comment)
+                .HasMaxLength(255);
+            builder.Property(r => r.Grade);
+        }
     }
 }
