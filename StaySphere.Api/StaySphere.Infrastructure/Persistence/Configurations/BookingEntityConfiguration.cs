bc6c97fbc609b412aa075ed11a9aa022623444a2
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-31 15:42:32 +0500

    Add BookingEntityConfiguration

--- StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/BookingEntityConfiguration.cs
+++ StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/BookingEntityConfiguration.cs
@@ -1,6 +1,37 @@
-﻿namespace StaySphere.Infrastructure.Persistence.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage;
+using StaySphere.Domain.Entities;
+
+namespace StaySphere.Infrastructure.Persistence.Configurations
 {
-    internal class BookingEntityConfiguration
+    public class BookingEntityConfiguration : IEntityTypeConfiguration<Booking>
     {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.ToTable(nameof(Booking));
+            builder.HasKey(b => b.Id);
+
+            builder.HasOne(b => b.Employee)
+                .WithMany(e => e.Bookings)
+                .HasForeignKey(b => b.EmployeeId);
+
+            builder.HasOne(b => b.Guest)
+                .WithMany(g => g.Bookings)
+                .HasForeignKey(b => b.GuestId);
+
+            builder.HasOne(b => b.Room)
+                .WithMany(r => r.Bookings)
+                .HasForeignKey(b => b.RoomId);
+
+            builder.Property(b => b.CheckInData)
+                .HasColumnType("date")
+                .IsRequired(true);
+            builder.Property(b => b.CheckOutData)
+                .HasColumnType("date")
+                .IsRequired(true);
+            builder.Property(b => b.TotalPrice)
+                .HasColumnType("money");
+        }
     }
 }
