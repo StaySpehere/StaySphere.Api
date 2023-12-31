c7d6ded36d3c1ab58d2fcc03dff0a516480bfa97
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-31 16:33:41 +0500

    Add CategoryEntityConfiguration

--- StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/CategoryEntityConfiguration.cs
+++ StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/CategoryEntityConfiguration.cs
@@ -1,6 +1,25 @@
-﻿namespace StaySphere.Infrastructure.Persistence.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StaySphere.Domain.Entities;
+
+namespace StaySphere.Infrastructure.Persistence.Configurations
 {
-    internal class CategoryEntityConfiguration
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category")
+                .HasKey(c => c.Id);
+
+            builder.HasMany(c => c.Rooms)
+                .WithOne(r => r.Category)
+                .HasForeignKey(r => r.CategoryId);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(255);
+            builder.Property(c => c.Price)
+                .HasColumnType("money");
+
+        }
     }
 }
