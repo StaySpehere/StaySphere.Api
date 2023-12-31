7bd1fcc0af8a885718099c5356d0574a5ae2433f
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-31 20:12:19 +0500

    Add DocumentEntityConfiguration

--- StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/DocumentEntityConfiguration.cs
+++ StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/DocumentEntityConfiguration.cs
@@ -1,6 +1,32 @@
-﻿namespace StaySphere.Infrastructure.Persistence.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Document = StaySphere.Domain.Entities.Document;
+
+namespace StaySphere.Infrastructure.Persistence.Configurations
 {
-    internal class DocumentEntityConfiguration
+    public class DocumentEntityConfiguration : IEntityTypeConfiguration<Document>
     {
+        public void Configure(EntityTypeBuilder<Document> builder)
+        {
+            builder.ToTable("Document")
+                 .HasKey(d => d.Id);
+
+            builder.HasMany(d => d.Guests)
+                .WithOne(g => g.Document)
+                .HasForeignKey(g => g.DocumentId);
+
+            builder.Property(d => d.SerialNumber);
+            builder.Property(d => d.FirstName)
+                .HasMaxLength(255);
+            builder.Property(d => d.LastName)
+                .HasMaxLength(255);
+            builder.Property(d => d.BrithDate)
+                .HasColumnType("date");
+            builder.Property(d => d.Gender)
+                .HasMaxLength(255);
+
+
+
+        }
     }
 }
