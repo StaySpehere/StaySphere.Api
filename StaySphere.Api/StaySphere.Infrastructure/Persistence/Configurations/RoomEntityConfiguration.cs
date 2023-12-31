7c982669b4adb9d3c2366f34a8f1ed0ef127d06f
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2023-12-31 20:50:33 +0500

    Add RoomEntityConfiguration

--- StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/RoomEntityConfiguration.cs
+++ StaySphere.Api/StaySphere.Infrastructure/Persistence/Configurations/RoomEntityConfiguration.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StaySphere.Domain.Entities;
 
 namespace StaySphere.Infrastructure.Persistence.Configurations
 {
-    internal class RoomEntityConfiguration
+    public class RoomEntityConfiguration : IEntityTypeConfiguration<Room>
     {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.ToTable(nameof(Room));
+            builder.HasKey(r => r.Id);
+
+            builder.HasOne(r => r.Category)
+                .WithMany(c => c.Rooms)
+                .HasForeignKey(r => r.CategoryId);
+            builder.HasMany(r => r.Bookings)
+                .WithOne(b => b.Room)
+                .HasForeignKey(b => b.RoomId);
+
+            builder.Property(r => r.Number);
+            builder.Property(r => r.Floor);
+        }
     }
 }
