de04fc383111ea004c756217ae8d79a54c763d9c
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-04 09:47:41 +0500

    Add CategoryResourceParameters

--- StaySphere.Api/StaySphere.Domain/ResourceParameters/BookinResourceParameters.cs
+++ StaySphere.Api/StaySphere.Domain/ResourceParameters/BookinResourceParameters.cs
@@ -4,10 +4,10 @@ namespace StaySphere.Domain.ResourceParameters
 {
     public class BookingResourceParameters : ResourceParametersBase
     {
-        public decimal TotalPrice { get; set; }
         public int GuestId { get; set; }
         public int EmployeeId { get; set; }
         public int RoomId { get; set; }
+        public decimal TotalPrice { get; set; }
         public decimal? PriceLessThan { get; set; }
         public decimal? PriceGreaterThan { get; set; }
     }
---
+++ StaySphere.Api/StaySphere.Domain/ResourceParameters/CategoryResourceParameters.cs
@@ -0,0 +1,11 @@
+﻿using StaySphere.Domain.Resource;
+
+namespace StaySphere.Domain.ResourceParameters
+{
+    public class CategoryResourceParameters : ResourceParametersBase
+    {
+        public decimal? Price { get; set; }
+        public decimal? PriceLessThan { get; set; }
+        public decimal? PriceGreaterThan { get; set; }
+    }
+}
