bf2de58774ade944ad2c68f3b19305350faa1e89
axamtovdiyorbek <128776299+diyorbekaxmatov@users.noreply.github.com>
2024-01-04 11:36:29 +0500

    Update PaginationExtensions.cs

--- StaySphere.Api/StaySphere.Domain/Pagination/PaginationExtensions.cs
+++ StaySphere.Api/StaySphere.Domain/Pagination/PaginationExtensions.cs
@@ -4,6 +4,19 @@ namespace StaySphere.Domain.Pagination
 {
     public static class PaginationExtensions
     {
+        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(
+            this IQueryable<T> source,
+            int pageSize,
+            int pageNumber) where T : EntityBase
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        }
+
         public static PaginatedList<T> ToPaginatedList<T>(
             this IQueryable<T> source,
             int pageSize,
