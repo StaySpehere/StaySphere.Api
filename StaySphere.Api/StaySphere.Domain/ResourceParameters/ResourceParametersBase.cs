c386f3f98f1d77bb7cb1dbea0a473058561e1cdb
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-04 09:29:39 +0500

    Add ResourceParametersBase

---
+++ StaySphere.Api/StaySphere.Domain/ResourceParameters/ResourceParametersBase.cs
@@ -0,0 +1,19 @@
+﻿namespace StaySphere.Domain.Resource
+{
+    public abstract class ResourceParametersBase
+    {
+        protected virtual int MaxPageSize { get; set; } = 25;
+
+        public virtual string? SearchString { get; set; }
+        public abstract string OrderBy { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 15;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+    }
+}
