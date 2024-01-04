3e956b6b807cae62f570d3b615acc8a0d6a33dc1
axamtovdiyorbek <128776299+diyorbekaxmatov@users.noreply.github.com>
2024-01-04 16:29:22 +0500

    Create-DbContext

--- StaySphere.Api/StaySphere.Domain/DTOs/Booking/BookingDto.cs
+++ StaySphere.Api/StaySphere.Domain/DTOs/Booking/BookingDto.cs
@@ -5,13 +5,9 @@ namespace StaySphere.Domain.DTOs.Booking
     public class BookingDto
     {
         public int Id { get; set; }
-
         public int GuestId { get; set; }
-
         public int EmployeeId { get; set; }
-
         public int RoomId { get; set; }
-
         public DateTime StayDuration { get; set; }
         public decimal TotalPrice { get; set; }
         public ICollection<ReviewDto> Reviews { get; set; }
