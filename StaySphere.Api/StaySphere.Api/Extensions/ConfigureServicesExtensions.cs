93bb264a7b77f4eeab24d9ac70a33142109f0d8c
Abror <nabiddinovabror8@gmail.com>
2024-01-09 10:18:44 +0500

    Add ConfigureRepositories

--- StaySphere.Api/StaySphere.Api/Extensions/ConfigureServicesExtensions.cs
+++ StaySphere.Api/StaySphere.Api/Extensions/ConfigureServicesExtensions.cs
@@ -1,9 +1,27 @@
 ﻿using Serilog;
+using StaySphere.Domain.Interfaces.Repositories;
+using StaySphere.Domain.Interfaces.Services;
+using StaySphere.Infrastructure.Persistence.Repositories;
 
 namespace StaySphere.Api.Extensions
 {
     public static class ConfigureServicesExtensions
     {
+        public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IDocumentRepository, DocumentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IGuestRepository, GuestRepository>();
+            services.AddScoped<IPositionRepository, PositionRepository>();
+            services.AddScoped<IReviewRepository, ReviewRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+       
+            return services;
+        }
+
+
         public static IServiceCollection ConfigureLogger(this IServiceCollection services)
         {
             Log.Logger = new LoggerConfiguration()
