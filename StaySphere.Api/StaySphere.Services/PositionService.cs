c2d0d0059715aff600ee3b992aa598920fd1a559
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-12 10:11:32 +0500

    Add loger,mapper, dbContext and create controller

--- StaySphere.Api/StaySphere.Services/PositionService.cs
+++ StaySphere.Api/StaySphere.Services/PositionService.cs
@@ -1,6 +1,22 @@
-﻿namespace StaySphere.Services
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using StaySphere.Domain.Interfaces.Repositories;
+using StaySphere.Domain.Interfaces.Services;
+using StaySphere.Infrastructure.Persistence;
+
+namespace StaySphere.Services
 {
-    public class PositionService
+    public class PositionService : IPositionService
     {
+        public readonly IMapper _mapper;
+        public readonly StaySphereDbContext _context;
+        public readonly ILogger<PositionService> _logger;
+
+        public PositionService(IMapper mapper, StaySphereDbContext context, ILogger<PositionService> logger)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
     }
 }
