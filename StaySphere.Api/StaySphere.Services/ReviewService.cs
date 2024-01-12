e91f0688361ad720df23b374f43c0d3827b1379d
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-12 22:09:16 +0500

    Add mapper,loger,dbCOntext and create controller

--- StaySphere.Api/StaySphere.Services/ReviewService.cs
+++ StaySphere.Api/StaySphere.Services/ReviewService.cs
@@ -1,8 +1,22 @@
-﻿using StaySphere.Domain.Interfaces.Services;
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using StaySphere.Domain.Interfaces.Services;
+using StaySphere.Infrastructure.Persistence;
 
 namespace StaySphere.Services
 {
     public class ReviewService : IReviewService
     {
+
+        public readonly IMapper _mapper;
+        public readonly StaySphereDbContext _context;
+        public readonly ILogger<ReviewService> _logger;
+
+        public ReviewService(IMapper mapper, StaySphereDbContext context, ILogger<ReviewService> logger)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
     }
 }
