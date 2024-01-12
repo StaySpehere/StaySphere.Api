2e25e1eede4bde9d13fc9bc86a01dadc1bb80e12
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-12 10:23:48 +0500

    Add GetGuests to PositionService

--- StaySphere.Api/StaySphere.Services/PositionService.cs
+++ StaySphere.Api/StaySphere.Services/PositionService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Logging;
-using StaySphere.Domain.Interfaces.Repositories;
+using StaySphere.Domain.DTOs.Guest;
+using StaySphere.Domain.DTOs.Position;
 using StaySphere.Domain.Interfaces.Services;
+using StaySphere.Domain.Pagination;
+using StaySphere.Domain.ResourceParameters;
 using StaySphere.Infrastructure.Persistence;
 
 namespace StaySphere.Services
@@ -17,6 +20,42 @@ namespace StaySphere.Services
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _context = context ?? throw new ArgumentNullException(nameof(context));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<PaginatedList<PositionDto>> GetPositions(PositionResourceParameters positionResourceParameters)
+        {
+            var query = _context.Positions.AsQueryable();
+            if (positionResourceParameters.Salary is not null)
+            {
+                query = query.Where(x => x.Salary == positionResourceParameters.Salary);
+            }
+
+            if (positionResourceParameters.SalaryLessThan is not null)
+            {
+                query = query.Where(x => x.Salary < positionResourceParameters.SalaryLessThan);
+            }
+
+            if (positionResourceParameters.SalaryGreaterThan is not null)
+            {
+                query = query.Where(x => x.Salary > positionResourceParameters.SalaryGreaterThan);
+            }
+
+            if (!string.IsNullOrEmpty(positionResourceParameters.OrderBy))
+            {
+                query = positionResourceParameters.OrderBy.ToLowerInvariant() switch
+                {
+                    "Name" => query.OrderBy(x => x.Name),
+                    "Namedesc" => query.OrderByDescending(x => x.Name),
+                    "Salary" => query.OrderBy(x => x.Salary),
+                    "Salarydesc" => query.OrderByDescending(x => x.Salary),
+                    _ => query.OrderBy(x => x.Name)
+                };
+            }
+
+            var positions = await query.ToPaginatedListAsync(positionResourceParameters.PageSize, positionResourceParameters.PageNumber);
+            var positionsDtos = _mapper.Map<List<PositionDto>>(positions);
+
+            return new PaginatedList<PositionDto>(positionsDtos, positions.TotalCount, positions.CurrentPage, positions.PageSize);
         }
     }
 }
