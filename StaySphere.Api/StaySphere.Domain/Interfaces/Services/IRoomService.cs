4b4def8c27946edcaf7243c553855ac113f3bfbe
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-12 22:46:06 +0500

    Corrected IRoomService

--- StaySphere.Api/StaySphere.Domain/Interfaces/Services/IRoomService.cs
+++ StaySphere.Api/StaySphere.Domain/Interfaces/Services/IRoomService.cs
@@ -1,10 +1,12 @@
 ﻿using StaySphere.Domain.DTOs.Room;
+using StaySphere.Domain.Pagination;
+using StaySphere.Domain.ResourceParameters;
 
 namespace StaySphere.Domain.Interfaces.Services
 {
     public interface IRoomService
     {
-        Task<RoomDto> GetRooms();
+        Task<PaginatedList<RoomDto>> GetRooms(RoomResourceParameters roomResourceParameters);
         Task<RoomDto?> GetRoomById(int id);
         Task<RoomDto> CreateRoom(RoomForCreateDto roomForCreateDto);
         Task UpdateRoom(RoomForUpdateDto roomForUpdateDto);
