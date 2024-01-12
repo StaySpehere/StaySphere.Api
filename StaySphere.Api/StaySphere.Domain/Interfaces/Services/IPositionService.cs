9cafa5b1c675cff1895f54eefe9b83ebf1bcab4f
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-12 22:07:12 +0500

    Corrected IPositionService

--- StaySphere.Api/StaySphere.Domain/Interfaces/Services/IPositionService.cs
+++ StaySphere.Api/StaySphere.Domain/Interfaces/Services/IPositionService.cs
@@ -1,10 +1,12 @@
 ﻿using StaySphere.Domain.DTOs.Position;
+using StaySphere.Domain.Pagination;
+using StaySphere.Domain.ResourceParameters;
 
 namespace StaySphere.Domain.Interfaces.Services
 {
     public interface IPositionService
     {
-        Task<PositionDto> GetPositions();
+        Task<PaginatedList<PositionDto>> GetPositions(PositionResourceParameters positionResourceParameters);
         Task<PositionDto?> GetPositionById(int id);
         Task<PositionDto> CreatePosition(PositionForCreateDto positionForCreateDto);
         Task UpdatePosition(PositionForUpdateDto positionForUpdateDto);
