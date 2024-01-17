7e6b7f7abd8b26dae6f63e6d6778ab48c56994bf
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-17 18:40:54 +0500

    Change UpdateDocument name to UpdateDocumentAsync name in IDocumentService

--- StaySphere.Api/StaySphere.Domain/Interfaces/Services/IDocumentService.cs
+++ StaySphere.Api/StaySphere.Domain/Interfaces/Services/IDocumentService.cs
@@ -9,7 +9,7 @@ namespace StaySphere.Domain.Interfaces.Services
         Task<PaginatedList<DocumentDto>>GetDocumentsAsync(DocumentResourceParameters documentResourceParameters);
         Task<DocumentDto?> GetDocumentByIdAsync(int id);
         Task<DocumentDto> CreateDocumentAsync(DocumentForCreateDto documentForCreateDto);
-        Task UpdateDocument(DocumentForUpdateDto documentForUpdateDto);
+        Task UpdateDocumentAsync(DocumentForUpdateDto documentForUpdateDto);
         Task DeleteDocument(int id);
     }
 }
