f939a58484bb775eefefc5007eeaefb20aa33302
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-19 22:16:57 +0500

    Change methods of RoomController to async

--- StaySphere.Api/StaySphere.Api/Controllers/RoomsController.cs
+++ StaySphere.Api/StaySphere.Api/Controllers/RoomsController.cs
@@ -19,47 +19,43 @@ namespace StaySphere.Api.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<RoomDto>> GetRoomsAsync(
+        public async Task<ActionResult<IEnumerable<RoomDto>>> GetRoomsAsync(
                [FromQuery] RoomResourceParameters roomResourceParameters)
         {
-            var rooms = _roomService.GetRooms(roomResourceParameters);
-
+            var rooms = await _roomService.GetRoomsAsync(roomResourceParameters);
             return Ok(rooms);
         }
 
         [HttpGet("{id}", Name = "GetRoomById")]
-        public ActionResult Get(int id)
+        public async Task<ActionResult> Get(int id)
         {
-            var room = _roomService.GetRoomById(id);
-
+            var room = await _roomService.GetRoomByIdAsync(id);
             return Ok(room);
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody] RoomForCreateDto room)
+        public async Task<ActionResult> Post([FromBody] RoomForCreateDto room)
         {
-            _roomService.CreateRoom(room);
-
+            await _roomService.CreateRoomAsync(room);
             return StatusCode(201);
         }
 
         [HttpPut("{id}")]
-        public ActionResult Put(int id, [FromBody] RoomForUpdateDto room)
+        public async Task<ActionResult> Put(int id, [FromBody] RoomForUpdateDto room)
         {
             if (id != room.Id)
             {
                 return BadRequest($"Route id: {id} does not match with parameter id: {room.Id}.");
             }
-            _roomService.UpdateRoom(room);
 
-            return NoContent();
+           await _roomService.UpdateRoomAsync(room);
+           return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            _roomService.DeleteRoom(id);
-
+            await _roomService.DeleteRoomAsync(id);
             return NoContent();
         }
     }
