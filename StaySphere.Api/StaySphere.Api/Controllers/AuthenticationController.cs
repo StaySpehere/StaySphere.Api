b8e7bd9cdf0467ed5019983c9df87a80726eb94f
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-19 22:38:24 +0500

    Create User class

--- StaySphere.Api/StaySphere.Api/Controllers/AuthenticationController.cs
+++ StaySphere.Api/StaySphere.Api/Controllers/AuthenticationController.cs
@@ -51,11 +51,4 @@ namespace StaySphere.Api.Controllers
             };
         }
     }
-    class User
-    {
-        public string? Name { get; set; }
-        public string? Phone { get; set; }
-        public string? Login { get; set; }
-        public string? Password { get; set; }
-    }
 }
--- StaySphere.Api/StaySphere.Api/Models/User.cs
+++ StaySphere.Api/StaySphere.Api/Models/User.cs
@@ -2,5 +2,9 @@
 {
     public class User
     {
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
     }
 }
