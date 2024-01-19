dff18ad5ec1373508738414145769b0c5d9b33b0
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-19 18:45:18 +0500

    Change GetPositionById name to GetPositionByIdAsync in IPositionService

--- StaySphere.Api/StaySphere.Domain/Interfaces/Services/IPositionService.cs
+++ StaySphere.Api/StaySphere.Domain/Interfaces/Services/IPositionService.cs
@@ -7,7 +7,7 @@ namespace StaySphere.Domain.Interfaces.Services
     public interface IPositionService
     {
         Task<PaginatedList<PositionDto>> GetPositionsAsync(PositionResourceParameters positionResourceParameters);
-        Task<PositionDto?> GetPositionById(int id);
+        Task<PositionDto?> GetPositionByIdAsync(int id);
         Task<PositionDto> CreatePosition(PositionForCreateDto positionForCreateDto);
         Task UpdatePosition(PositionForUpdateDto positionForUpdateDto);
         Task DeletePosition(int id);
