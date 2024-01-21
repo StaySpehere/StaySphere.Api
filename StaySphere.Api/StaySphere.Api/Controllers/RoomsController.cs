1f95080f10313983e8c575268bace6c4a3299200
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-01-21 12:20:17 +0500

    Adding Pagination to RoomsController

--- StaySphere.Api/StaySphere.Api/Controllers/RoomsController.cs
+++ StaySphere.Api/StaySphere.Api/Controllers/RoomsController.cs
@@ -1,9 +1,13 @@
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StaySphere.Domain.DTOs.Category;
 using StaySphere.Domain.DTOs.Room;
+using StaySphere.Domain.Entities;
 using StaySphere.Domain.Interfaces.Services;
+using StaySphere.Domain.Pagination;
 using StaySphere.Domain.ResourceParameters;
+using System.Text.Json;
 
 namespace StaySphere.Api.Controllers
 {
@@ -23,6 +27,10 @@ namespace StaySphere.Api.Controllers
                [FromQuery] RoomResourceParameters roomResourceParameters)
         {
             var rooms = await _roomService.GetRoomsAsync(roomResourceParameters);
+
+            var metaData = await GetPaginationMetaDataAsync(rooms);
+
+            Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(metaData));
             return Ok(rooms);
         }
 
@@ -57,6 +65,16 @@ namespace StaySphere.Api.Controllers
         {
             await _roomService.DeleteRoomAsync(id);
             return NoContent();
+        }
+        private async Task<PagenationMetaData> GetPaginationMetaDataAsync(PaginatedList<RoomDto> roomDtos)
+        {
+            return new PagenationMetaData
+            {
+                Totalcount = roomDtos.TotalCount,
+                PageSize = roomDtos.PageSize,
+                CurrentPage = roomDtos.CurrentPage,
+                TotalPages = roomDtos.TotalPages,
+            };
         }
     }
 }
